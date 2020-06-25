0de7d892f20898cfda44ad71e35a0a537d06d70d
Vimal Nathan <vimal.olaganathan@gmail.com>
2020-06-24 20:38:09 -0400

    Fixing time-scraper by adding ts-util dependency

--- time-scraper/time-scraper.js
+++ time-scraper/time-scraper.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch');
 const scriptSettings = require('../trelloAuthSettings.json');
 const trelloRestUtil = require('../common/trello-rest-util');
 const textReportUtil = require('../common/text-report-util');
+const tsUtil = require('./ts-util');
 
 // Program starts here
 trelloRestUtil.getListOfBoards(fetch, scriptSettings, textReportUtil.createTextReport, tsUtil.projectMapObjectGenerator);
